78767ae6d4f85043bdc6a8f13409cc83a76e5b17
Tusharyadav21 <Tusharydv@hotmail.com>
2023-01-09 00:50:53 +0530

    1st save

--- src/App.js
+++ src/App.js
@@ -1,34 +1,52 @@
-import React, { useState, useRef } from 'react';
+import React from "react";
+import { useState, useRef } from "react";
+import { useEffect } from "react/cjs/react.production.min";
+import Loader from "./Component/loader/Loader";
+
+// Components
 // import Weather from './Component/Weather';
-import styles from './App.Module.css';
 // import MessageBox from './Component/MessageBox/MessageBox';
 // import Modal from './Component/Modal/Modal';
 
+import styles from "./App.Module.css";
 
 function App() {
+	// const [show, setShow] = useState(false)
+	// const [head, setHead] = useState("HEllo");
+	// const [text, setText] = useState("world");
 
-  // const [show, setShow] = useState(false)
+	// const MessageRef = useRef(null)
 
-  // const MessageRef = useRef(null)
+	// handleSubmit = (e) => {
+	// 	console.log("CLicked");
+	// };
 
-  return (
-    <>
-      <div className={styles.body}>
-        {/* Message */}
+	// useEffect(() => console.log(head + " " + text), [text]);
 
+	return (
+		<>
+			<Loader />
+			{/* <div>Count</div> */}
+			{/* <div>{head}</div> */}
+			{/* <div className={styles.body}> */}
+			{/* Message */}
 
+			{/* <h1 style={fun}>React App</h1> */}
+			{/* <p style={{ fontSize: "34" }}>React App</p> */}
+			{/* <h1>React App</h1> */}
 
-        {/* Modal */}
-        {/* <button onClick={() => setShow(true)}>Show Modal</button>
+			{/* <button onClick={handleSubmit}>Press</button> */}
+
+			{/* Modal */}
+			{/* <button onClick={() => setShow(true)}>Show Modal</button>
         <Modal onClose={() => setShow(false)} onOk={() => setShow(false)} show={show} /> */}
 
-
-        {/* Message Box */}
-        {/* <button onClick={() => { MessageRef.current.show() }}>Message</button>
+			{/* Message Box */}
+			{/* <button onClick={() => { MessageRef.current.show() }}>Message</button>
         <MessageBox ref={MessageRef} message="Successful Notification" type="Success" /> */}
-      </div>
-    </>
-  );
+			{/* </div> */}
+		</>
+	);
 }
 
 export default App;
---
+++ src/Component/loader/Loader.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./loader.css";
+
+const Loader = () => {
+	return (
+		<div>
+			<div class='frame'>
+				<div class='center'>
+					<div class='dot-1'></div>
+					<div class='dot-2'></div>
+					<div class='dot-3'></div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Loader;
